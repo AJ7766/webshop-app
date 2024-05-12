4f9cd91b7e5e86465a432543bffe4ac19e63e4f8
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-05-12 14:16:58 +0200

    getting data from db

--- prisma/migrations/20240508161656_init/migration.sql
+++ prisma/migrations/20240508161656_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "Product" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
-    "PriceinSEK" INTEGER NOT NULL,
+    "priceinSEK" INTEGER NOT NULL,
     "imagePath" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "isAvailableForPurchase" BOOLEAN NOT NULL DEFAULT true,
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,66 @@
-export default function AdminDashboard() {
-    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import db from "@/db/db";
+
+
+async function getSalesData() {
+    const data = await db.order.aggregate({
+        _sum: { pricePaidinSEK: true },
+        _count: true
+    })
+
+    return {
+        amount: data._sum.pricePaidinSEK || 0,
+        numberOfSales: data._count
+    }
+}
+
+async function getUserData() {
+    const [userCount, orderData] = await Promise.all([
+        db.user.count(),
+        db.order.aggregate({
+        _sum: { pricePaidinSEK: true },
+        }),
+    ])
+
+    return {
+        userCount,
+        averageValuePerPerson: userCount === 0 ? 0 : (orderData._sum.pricePaidinSEK)
+    }
+}
+export default async function AdminDashboard() {
+    const [salesData, userData] = await Promise.all([
+        getSalesData(),
+        getUserData()
+    ])
+
+    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <DashboardCard
+            title="Sales"
+            subtitle={`${salesData.numberOfSales} Orders`}
+            body={`${salesData.amount} SEK`} 
+        />
+        <DashboardCard
+            title="Customer"
+            subtitle={`${userData.averageValuePerPerson} Average Value`}
+            body={`${userData.userCount} Customers`} 
+        />
+    </div>
+}
+
+type DashboardCardProps = {
+    title: string
+    subtitle: string
+    body: string
+}
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+    return <Card>
+        <CardHeader>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{subtitle}</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <p>{body}</p>
+        </CardContent>
+    </Card>
 }
